3645a2a42c3db88b88541ba418961cc582441e35
Luc Constantin <lucc.constantin@gmail.com>
2023-11-15 17:39:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('vat-calculator-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var vatRate = document.getElementById('vat-rate').value;
+    var amount = document.getElementById('amount').value;
+
+   
+    if (isNaN(amount)) {
+        alert('Please enter a valid number for the amount.');
+        return;
+    }
+
+    var vat = (vatRate / 100) * amount;
+    var total = parseFloat(amount) + parseFloat(vat);
+
+    document.getElementById('vat').innerHTML = vat.toFixed(2);
+    document.getElementById('total').innerHTML = total.toFixed(2);
+    document.getElementById('results').style.display = '';
+});
