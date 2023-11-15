53adbd5122063e8fd4224d44ca372ff290c203c6
Luc Constantin <lucc.constantin@gmail.com>
2023-11-15 19:01:46 +0100

    Update script.js
    added a reset button

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+function resetCalculator() {
+  
+    document.getElementById('vat-rate').value = '21';
+    document.getElementById('amount').value = '';
+    document.getElementById('results').style.display = 'none';
+}
+
 document.getElementById('vat-calculator-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     var vatRate = document.getElementById('vat-rate').value;
     var amount = document.getElementById('amount').value;
 
-   
     if (isNaN(amount)) {
         alert('Please enter a valid number for the amount.');
         return;
